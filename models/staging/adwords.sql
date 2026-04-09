bc0f53557d1f4a948166404eef6d2317c1095233
yudum ergün <yudumergunn@gmail.com>
2026-04-09 16:59:44 +0000

    commt

--- models/staging/adwords.sql
+++ models/staging/adwords.sql
@@ -1,8 +1,19 @@
-select date_date,
-paid_source,
-campaign_key,
-camPGN_name as campaign_name,
-cast(ads_cost as float64) as ads_cost,
-impression,
-click
-from {{source('raw',adwords)}}
+
+with 
+source as (
+    select * from{{source('raw','adwords')}}
+)
+
+renamed as (
+    	select
+		date_date,
+		paid_source,
+		campaign_key,
+		campgn_name as campaign_name,
+		CAST(ads_cost as float64) as ads_cost,
+		impression,
+		click
+	from source
+)
+
+select * from renamed
